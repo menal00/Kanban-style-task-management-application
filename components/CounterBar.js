fdeb61837dd2d0dada9fcb44fac4a37d47f8104f
menal00 <menalmesuod00@gmail.com>
2026-04-01 06:53:25 -0400

    Add highPriorityTasks to CounterBar component

--- components/CounterBar.js
+++ components/CounterBar.js
@@ -1,10 +1,17 @@
-function CounterBar({ totalBoards, totalTasks, overdueTasks, dueSoonTasks }) {
+function CounterBar({
+  totalBoards,
+  totalTasks,
+  overdueTasks,
+  dueSoonTasks,
+  highPriorityTasks
+}) {
   return (
     <div className="counter-bar">
       <div className="counter-box">Boards: {totalBoards}</div>
       <div className="counter-box">Tasks: {totalTasks}</div>
       <div className="counter-box">Overdue: {overdueTasks}</div>
       <div className="counter-box">Due Soon: {dueSoonTasks}</div>
+      <div className="counter-box">High Priority: {highPriorityTasks}</div>
     </div>
   );
 }
