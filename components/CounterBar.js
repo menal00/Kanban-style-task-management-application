b36ed800272e2b1121f66755786e6d3cd278f660
menal00 <menalmesuod00@gmail.com>
2026-03-31 23:06:23 -0400

    Add CounterBar component to display task counters

--- components/CounterBar.js
+++ components/CounterBar.js
@@ -1,1 +1,10 @@
-
+function CounterBar({ totalBoards, totalTasks, overdueTasks, dueSoonTasks }) {
+  return (
+    <div className="counter-bar">
+      <div className="counter-box">Boards: {totalBoards}</div>
+      <div className="counter-box">Tasks: {totalTasks}</div>
+      <div className="counter-box">Overdue: {overdueTasks}</div>
+      <div className="counter-box">Due Soon: {dueSoonTasks}</div>
+    </div>
+  );
+}
